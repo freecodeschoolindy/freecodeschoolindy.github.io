b24c6095a0227e2447c20bbc5b058c6ea63b2863
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-11-16 13:32:31 -0500

    setup vuex

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+Vue.config.devtools = true;
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    theme: 'Yeti'
+  },
+  mutations: {
+    setTheme: function (state, theme) {
+      state.theme = theme;
+    }
+  },
+  actions: {},
+  getters: {}
+});
+
 // eslint-disable-next-line no-unused-vars
 const app = new Vue({
   el: '#app',
   template: '<app-composition></app-composition>',
   components: {
     'app-composition': httpVueLoader('components/app-composition.vue')
-  }
+  },
+  store: store
 });
