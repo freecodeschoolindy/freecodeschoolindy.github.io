f0426aafd6e8636fc2f50f0b3f518abd89b0ec61
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-03-11 22:50:41 -0400

    Update LiveStream.vue (#34)

--- components/LiveStream.vue
+++ components/LiveStream.vue
@@ -1,13 +1,17 @@
 <template>
   <div v-if="streamer === 'jared'">
-
+    <iframe
+      allowfullscreen
+      scrolling="no"
+      src="https://player.twitch.tv/?channel=thejaredwilcurt"
+    ></iframe>
   </div>
   <div v-else>
     <iframe
-      src="https://www.youtube-nocookie.com/embed/BHAe8Nfef4Q"
-      frameborder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen
+      scrolling="no"
+      src="https://www.youtube-nocookie.com/embed/live_stream?channel=UCxA99Yr6P_tZF9_BgtMGAWA"
     ></iframe>
   </div>
 </template>
@@ -35,4 +39,10 @@ module.exports = {
 </script>
 
 <style scoped>
+iframe {
+  width: 100%;
+  height: calc(100vh - 141px);
+  border: 0px;
+  margin-bottom: -85px;
+}
 </style>
