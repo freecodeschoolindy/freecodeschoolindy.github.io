adfeb1609be51638b15a81932c21d68db5015cf2
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-03-12 09:06:15 -0400

    Streaming

--- components/LiveStream.vue
+++ components/LiveStream.vue
@@ -1,36 +1,50 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <span class="_margin-top-2">
+  <i-container>
+    <i-row>
+      <i-column xs="12" md="6">
+        <i-form-group class="_margin-top-1">
+          <i-form-label><strong>Streamers:</strong></i-form-label>
+          <div>
+            <i-button-group>
+              <i-button
+                v-for="(streamer, key) in streamers"
+                :variant="selectedStreamer === key ? 'primary' : 'light'"
+                @click="selectedStreamer = key"
+                :key="'streamer' + key"
+              >
+                {{ streamer.name }}
+              </i-button>
+            </i-button-group>
+          </div>
+        </i-form-group>
+
+        <div class="_margin-top-2">
           To participate in chat click here (account creation/login required).
-        </span>
-        <a
-          :href="chatLink"
-          class="btn btn-lg btn-primary _margin-top-1"
-        >
-          Join chat!
-        </a>
-      </div>
-      <div class="col-md-6">
+        </div>
+
+        <div class="_text-center">
+          <i-button
+            :href="chatLink"
+            class="_margin-top-1"
+            outline
+            tag="a"
+            variant="primary"
+          >
+            Join chat!
+          </i-button>
+        </div>
+      </i-column>
+      <i-column xs="12" md="6">
         <iframe
-          v-if="streamer === 'jared'"
-          allowfullscreen
-          class="iframe"
-          scrolling="no"
-          src="https://player.twitch.tv/?channel=thejaredwilcurt"
-        ></iframe>
-        <iframe
-          v-else
+          :src="iframeSrc"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
           class="iframe"
           scrolling="no"
-          src="https://www.youtube-nocookie.com/embed/live_stream?channel=UCxA99Yr6P_tZF9_BgtMGAWA"
         ></iframe>
-      </div>
-    </div>
-  </div>
+      </i-column>
+    </i-row>
+  </i-container>
 </template>
 
 <script>
@@ -41,16 +55,35 @@ module.exports = {
   },
   data: function () {
     return {
-      streamer: 'gwen'
+      selectedStreamer: 'gwen',
+      streamers: {
+        gwen: {
+          name: 'Gwen Faraday',
+          url: 'https://www.youtube.com/channel/UCxA99Yr6P_tZF9_BgtMGAWA',
+          src: 'https://www.youtube-nocookie.com/embed/live_stream?channel=UCxA99Yr6P_tZF9_BgtMGAWA'
+        },
+        jared: {
+          name: 'The Jared Wilcurt',
+          url: 'https://twitch.tv/TheJaredWilcurt',
+          src: 'https://player.twitch.tv/?channel=thejaredwilcurt'
+        }
+      }
     };
   },
   computed: {
     chatLink: function () {
-      let streamers = {
-        gwen: 'https://www.youtube.com/channel/UCxA99Yr6P_tZF9_BgtMGAWA',
-        jared: 'https://twitch.tv/TheJaredWilcurt'
-      };
-      return streamers[this.streamer] || streamers.gwen;
+      let link = this.streamers.gwen.url;
+      if (this.streamers.hasOwnProperty(this.selectedStreamer)) {
+        link = this.streamers[this.selectedStreamer] && this.streamers[this.selectedStreamer].url || link;
+      }
+      return link;
+    },
+    iframeSrc: function () {
+      let link = this.streamers.gwen.src;
+      if (this.streamers.hasOwnProperty(this.selectedStreamer)) {
+        link = this.streamers[this.selectedStreamer] && this.streamers[this.selectedStreamer].src || link;
+      }
+      return link;
     }
   }
 };
