31eb84af7647d750865439ff4bc6445ec7f0f639
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-04-04 10:32:12 -0400

    Pin footer

--- components/app-composition.vue
+++ components/app-composition.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <the-navigation></the-navigation>
-    <div>
+    <the-navigation class="shrink"></the-navigation>
+    <div class="grow">
       <router-view></router-view>
     </div>
-    <the-footer></the-footer>
+    <the-footer class="shrink"></the-footer>
   </div>
 </template>
 
@@ -19,6 +19,17 @@ module.exports = {
 </script>
 
 <style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.shrink {
+  flex-shrink: 0;
+}
+.grow {
+  flex-grow: 1;
+}
 h1, h2, h3 {
   color: #345578;
   font-family: 'Roboto Mono', monospace;
